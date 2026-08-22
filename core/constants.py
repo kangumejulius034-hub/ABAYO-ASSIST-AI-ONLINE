ac6badf2d3023f29ed8641e354afea188d5e48c3
kangumejulius034-hub <kangumejulius034@gmail.com>
2026-08-23 00:01:13 +0400

    Release ABAYO command center v0.8.0

--- core/constants.py
+++ core/constants.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 APP_NAME = "ABAYO"
-APP_VERSION = "0.7.1"
+APP_VERSION = "0.8.0"
 
 MACHINE_STATUSES = ("Online", "Offline", "Maintenance")
 SUPPORTED_MACHINE_MODELS = ("Pakona PFS AG",)
